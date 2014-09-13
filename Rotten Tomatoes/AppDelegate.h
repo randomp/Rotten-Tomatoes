1a7d5567909546c678307e33f46e899175b146cd
peiqi <peiqi@yahoo-inc.com>
2014-09-13 14:28:19 -0700

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Peiqi Zheng on 9/13/14.
+//  Copyright (c) 2014 Peiqi Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
